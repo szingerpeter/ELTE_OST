180e2b550e4c69e666e5262700afcaf4ee134b52
Giorgio Segalla <giorgiosgl1997@gmail.com>
2020-12-23 18:31:10 +0100

    merge container data ingestion and forecasting

--- data_ingestion/package.sh
+++ data_ingestion/package.sh
@@ -1,3 +1,8 @@
 # sbt
+# preprocess python script
+echo "running preprocessing script"
+python src/main/python/preprocess.py
+
+
 echo "packaging scala source code data_ignestion"
 sbt package
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,36 @@
 # unzip data source
 echo "unzipping file"
 unzip -q mounted_data/2018_Electric_power_data.zip -d data/
-mv data/2018\ Electric\ power\ data data/2018_electric_power_data
+mv data/2018\ Electric\ power\ data data_ingestion/data/2018_electric_power_data
 # hadoop classpath
 #export SPARK_DIST_CLASSPATH=$(hadoop classpath)
-# preprocess python script
+
+# package all scala projects
+# sbt
+cd forecasting
+echo $(pwd)
+echo "packaging scala source code forecasting"
+sbt package -Dsbt.rootdir=true
+cd ..
+
+
+cd data_ingestion
 echo "running preprocessing script"
 python src/main/python/preprocess.py
 
-# package all scala projects
-/bin/bash forecasting/package.sh
-/bin/bash data_ingestion/package.sh
+echo "packaging scala source code data_ignestion"
+sbt package
+cd ..
 
-echo "keep container running"
-/bin/bash
+rmdir data
+
+
+while true; do
+    read -p "Do you wish bash(Bb) or fish(Ff)?" fb
+    case $fb in
+        [Ff]* ) apt-get install fish; fish; break;;
+        [Bb]* ) /bin/bash; exit;;
+        * ) echo "Please answer Bb or Ff.";;
+    esac
+done
+
