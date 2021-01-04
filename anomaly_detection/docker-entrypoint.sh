ffa5f76fb9ec75b90055268578490da93a383664
lenzomania <marc.lenz.math@gmail.com>
2021-01-04 21:31:26 +0100

    added wait for it sh

--- anomaly_detection/docker-entrypoint.sh
+++ anomaly_detection/docker-entrypoint.sh
@@ -5,6 +5,12 @@ cd $SPARK_HOME/conf
 cp log4j.properties.template log4j.properties
 cd -
 
-echo "start streaming"
+
+#!/bin/bash
+echo "waiting for kafka service"
+./wait-for-it.sh kafka:9093 --timeout=100 -- echo "kafka is up"
+
+
+echo "start anomaly detection service"
 spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 src/detection_process.py
 
